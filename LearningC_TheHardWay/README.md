a44352a61e2413310310434b19b9ce6df058ae5d
Joe-metry <joemetry150@gmail.com>
2023-01-17 10:58:59 +0100

    Learning C the hard way

---
+++ LearningC_TheHardWay/README.md
@@ -0,0 +1,1 @@
+this is a Readme on more of my C practice from a book called Learning C the hard way
---
+++ LearningC_TheHardWay/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("Today I learnt about the puts function in C,");
+	puts("My instructor asked that I print five lines using the puts function.");
+	puts("And so far I am having fun doing it.");
+	puts("And here comes the fifth line, so bye for now world!");
+
+	return 0;
+
+}
