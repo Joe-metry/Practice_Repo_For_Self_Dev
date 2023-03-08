0d8ac91826f1565ba313a14fa3dfd48a3f84652a
Joe-metry <joemetry150@gmail.com>
2023-03-08 16:15:15 +0100

    Python progress Wednesday 8th March, 2023

---
+++ Jenny_python/ClassDataType.py
@@ -0,0 +1,11 @@
+#I will be using the type function to know the datatypes of various data
+
+Name = "Joemetry"
+Age = 24
+Score = 89.91
+Grade = 'A'
+
+print(type(Name))
+print(type(Age))
+print(type(Score))
+print(type(Grade))
