8aef724a469cc330ff044f7460293e2f38e8e719
Joe-metry <joemetry150@gmail.com>
2023-03-08 15:50:38 +0100

    Update on my python studies Wednesday 8th March, 2023

---
+++ Jenny_python/swap.py
@@ -0,0 +1,15 @@
+#this program swaps two numbers entered by the user
+
+#The print function takes strings as arguments while the
+#input function returns a string, this means line 14 is
+#valid since two strings can be concatenated use the '+'
+#operator.
+
+x = input("Enter 1st number: ")
+y = input("Enter 2nd number: ")
+
+first_number = y
+second_number = x
+
+print("First number is now:" + first_number)
+print("Second number is now:", second_number)
