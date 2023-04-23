9bb17cd1d1e435eef80126fd78eb99b9e394b843
Joe-metry <joemetry150@gmail.com>
2023-04-23 21:06:18 +0100

    Exercise on lists of dictionaries

---
+++ Python_Book_Exercises/moreDicts1.py
@@ -0,0 +1,16 @@
+'''
+This is my practice exercise on lists
+of dictionaries. More updates on this
+will follow soon.
+'''
+
+list = [
+  {
+    1: "data",
+    "course": "Software Engineering",
+  },
+  {
+    1: 37000370001,
+    "salary": 4101429,
+  },
+  ]
