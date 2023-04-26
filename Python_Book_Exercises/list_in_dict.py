1360a23a7beb83a106f4d6c64798f097c8557f2f
Joe-metry <joemetry150@gmail.com>
2023-04-26 17:06:47 +0100

    More python practice

---
+++ Python_Book_Exercises/list_in_dict.py
@@ -0,0 +1,8 @@
+details = {
+  "nickname": "Mick",
+  "married": "yes",
+  "careers": ["firefighter", "mathematician", "rock star"],
+}
+
+if details["married"] == "yes" and "rock star" in details["careers"]:
+  print("Uh-oh")
