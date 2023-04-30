1ff0db6fe787c9c90be0b7e4c6271b69f507de30
Joe-metry <joemetry150@gmail.com>
2023-04-30 18:38:25 +0100

    Exercise on dictionaries

---
+++ Python_Book_Exercises/dictDict.py
@@ -0,0 +1,27 @@
+details = {
+  "nickname": "Mick",
+  "married": "yes",
+  "careers": ["firefighter", "mathematician", "rock star"],
+}
+
+if details["married"] == "yes" and "rock star" in details["careers"]:
+  print("Uh-oh")
+
+  dict = {
+ 0: {
+    "first_name": "ortiz",
+  },
+  1: {
+    "last_name": "Joel"
+  },
+}
+
+print(dict[0]["name"])
+
+dict = {
+  0: [8, "Iniesta"],
+  "key": {
+    "career": "software Engineering",
+    7: "Joseph",
+  },
+}
