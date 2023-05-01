dbd6f02435b30be541c3ddcc7d745ecebbeb8804
Joe-metry <joemetry150@gmail.com>
2023-05-01 06:55:05 -0700

    The function displays two default parameters on separate lines.

---
+++ Python_Book_Exercises/functprat4.py
@@ -0,0 +1,11 @@
+'''
+  Code a function with two parameters that have defaults.
+  The function displays the parameters on separate lines.
+  Call the function, overriding both defaults.
+'''
+
+def defaultParameters(i = "Hello", j = "World!"):
+  print(i)
+  print(j)
+
+defaultParameters(i = 'Software', j = 'Engineering')
