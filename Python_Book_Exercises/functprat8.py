359d36bf1a83e38d25ee840820ae918d07f74cb2
Joe-metry <joemetry150@gmail.com>
2023-05-01 07:02:21 -0700

    a three-line function that creates a list with three integers.

---
+++ Python_Book_Exercises/functprat8.py
@@ -0,0 +1,10 @@
+'''
+  Code a three-line function that creates a list with three integers.
+  Pass the list back to the calling code. Make everything up.
+'''
+
+def listCreator(a, b, c):
+  list = [a, b, c]
+  return list
+
+print(listCreator(3, 4, 5))
