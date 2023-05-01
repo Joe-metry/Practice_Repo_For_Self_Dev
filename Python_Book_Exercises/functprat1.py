11b51c0056c51d99627239aa106fb24b9ee42f97
Joe-metry <joemetry150@gmail.com>
2023-05-01 06:42:47 -0700

    The function does an arithmetic operation using

---
+++ Python_Book_Exercises/functprat1.py
@@ -0,0 +1,12 @@
+'''
+Code a function that has two parameters, both of them numbers.
+Assign a default value to each parameter.
+The function does an arithmetic operation using
+the two numbers and displays the result.
+'''
+
+def arithmetics(x = 10, j = 7):
+  print("$",x ** j)
+  
+arithmetics()
+
