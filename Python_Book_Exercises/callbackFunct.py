d10b66749d53fc630e88cf3b7f68e4f6af800f0a
Joe-metry <joemetry150@gmail.com>
2023-05-02 08:55:47 -0700

    Call back function

---
+++ Python_Book_Exercises/callbackFunct.py
@@ -0,0 +1,8 @@
+'''
+Call a function, assigning its returned value to a variable. 
+he function's argument is another function with one
+positional argument, a string. Make everything up.
+
+'''
+
+retVal = calledFunc(anotherFunct("Literal Data"))
