6a43d0d0fcad8a24fed22d57cdf96273c57ddb0b
Joe-metry <joemetry150@gmail.com>
2023-05-07 06:47:08 -0700

    More on two parameters

---
+++ Python_Book_Exercises/moreFu.py
@@ -0,0 +1,11 @@
+'''
+    Code a function that has two parameters that are lists.
+    As long as the first list is shorter than the second list,
+    append an element, "empty", to the first list.
+    Make up the names of the function and variables.
+'''
+
+def twoPara(listA, listY):
+  while len(listA) < len(listY):
+  listA.append("empty")
+
