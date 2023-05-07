63f962da201a5b7e16172328cb7ac6ceff523578
Joe-metry <joemetry150@gmail.com>
2023-05-07 06:52:09 -0700

    More while looping

---
+++ Python_Book_Exercises/valueAnother.py
@@ -0,0 +1,10 @@
+''
+As long as a variable holding an integer is less than the value of another
+variable, run a for loop that loops through all the elements of a list and
+adds each element of the list to the first variable. Use the concise approach
+to increment the first variable. Make everything up.
+'''
+
+while var < rav::
+  for i in theList:
+    var += i
