802921dd85faa12a6bc0385cac0b39cbfb83b75d
Joe-metry <joemetry150@gmail.com>
2023-05-08 14:19:31 -0700

    class with two attributes

---
+++ Python_Book_Exercises/classAtrriA.py
@@ -0,0 +1,12 @@
+'''
+    Code a class that has two attributes.
+    Then code an instance of the class.
+    Both attributes are strings. Make everything up.
+    '''
+
+    class TwoAttr():
+  def __init__(self, music, sports):
+    self.music = music
+    self.sports = sports
+
+talents011 = TwoAttr("Afro pop", "football")
