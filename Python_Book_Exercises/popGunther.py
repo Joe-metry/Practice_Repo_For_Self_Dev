84df33e9d54e2fdc895d81d82fae7f474e8bf9a8
Joe-metry <joemetry150@gmail.com>
2023-05-08 08:54:33 -0700

    Popped from a list using index

---
+++ Python_Book_Exercises/popGunther.py
@@ -0,0 +1,11 @@
+'''
+
+friends = ["Ross", "Rachel", "Monica", "Phoebe", "Gunther", "Joey", "Chandler"]
+Pop "Gunther" off the list and assign it to a variable.
+Display the variable.
+'''
+friends = ["Ross", "Rachel", "Monica", "Phoebe", "Gunther", "Joey", "Chandler"]
+
+guntherPopped = friends.pop(4)
+
+print(guntherPopped)
