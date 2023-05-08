1442cbe44233de03281a320c8de248bdc8c989fc
Joe-metry <joemetry150@gmail.com>
2023-05-08 14:20:50 -0700

    A class with three attributes

---
+++ Python_Book_Exercises/ThreeAttr.py
@@ -0,0 +1,10 @@
+'''
+      Code another class with three attributes. Make everything up.
+    '''
+
+    class Two():
+  def __init__(self, pay, level, position):
+    self.pay = pay
+    self.level = level
+    self.position = position
+
