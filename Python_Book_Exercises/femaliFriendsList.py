35e1c8cd8be20918cb7d0e597078c7f2364093d9
Joe-metry <joemetry150@gmail.com>
2023-05-08 08:55:58 -0700

    Prints the sliced list with only female friends

---
+++ Python_Book_Exercises/femaliFriendsList.py
@@ -0,0 +1,15 @@
+   '''
+    friends = ["Ross", "Rachel", "Monica", "Phoebe", "Joey", "Chandler"]
+
+    We're not ready to live-code a class definition yet,
+    so let's review earlier material: Slice consecutive elements
+    out of this list to create a new list, female_friends.
+    The elements of the new list will be:
+    "Rachel", "Monica", and "Phoebe".
+
+    '''
+
+friends = ["Ross", "Rachel", "Monica", "Phoebe", "Joey", "Chandler"]
+female_friends = friends[1:4]
+print(female_friends)
+
