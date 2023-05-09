022f1a8b5049637815f9f03f85069dab81641366
Joe-metry <joemetry150@gmail.com>
2023-05-09 10:37:12 -0700

    File input A

---
+++ Python_Book_Exercises/niceFileIO.py
@@ -0,0 +1,7 @@
+'''
+  Open a text file that's in a different folder than the
+  Python program that's opening it.
+  Assume you're in Windows. Make everything up.
+    '''
+
+    with open("HDX\dataCard.txt", "w") as dataUpdate:
