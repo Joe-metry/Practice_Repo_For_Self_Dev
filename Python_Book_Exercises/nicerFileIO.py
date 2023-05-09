1648eb6cfede523963e4fc9d171d1bdb6fb8ec1d
Joe-metry <joemetry150@gmail.com>
2023-05-09 10:38:39 -0700

    initialized File I/O exercise

---
+++ Python_Book_Exercises/nicerFileIO.py
@@ -0,0 +1,7 @@
+'''
+    Open a text file that's in a different folder than
+    the Python program that's opening it.
+    Assume you're in OS X or Linux. Make everything up.
+  '''
+
+with open("VelAcc/speed.txt", "w") as acce_upgrade:
