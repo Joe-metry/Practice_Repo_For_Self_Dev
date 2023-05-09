c6f73be1ee771091e889779775e47402c869fd76
Joe-metry <joemetry150@gmail.com>
2023-05-09 05:13:18 -0700

    This is an update to what I originally wanted to add to remote

---
+++ Python_Book_Exercises/FouLineCls.py
@@ -0,0 +1,12 @@
+'''
+  Code the first four lines of a class definition. It has one attribute.
+  The fourth line—and the last one that you're going to code—is the
+  first line of a method. Make everything up. Indent correctly.
+'''
+
+class products():
+  def __init__(self, export):
+    self.export = export
+
+  def goodsExp(self):
+
