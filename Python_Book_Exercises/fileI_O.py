b9ad7cc58c77ac654141cc9273253993ce2016e6
Joe-metry <joemetry150@gmail.com>
2023-05-12 04:42:40 -0700

    Continous practice of file I/O

---
+++ Python_Book_Exercises/fileI_O.py
@@ -0,0 +1,7 @@
+'''
+  More on data files.
+  file handling I/O
+  '''
+
+with open("index.html", "a") as f:
+  g = f.write("\n\n<!--This comment was added from the terminal during my file handling practice.-->")
