c26dc0fa6adccc0844fadccddae366892fb71a5a
Joe-metry <joemetry150@gmail.com>
2023-05-12 08:38:59 -0700

    Started modules, first exercise

---
+++ Python_Book_Exercises/modA.py
@@ -0,0 +1,18 @@
+'''
+Code a function to be included in the calculations module.
+It accepts two integers as parameters. In a single line,
+it adds the two integers together and returns the result.
+Call the function, passing two integers as arguments.
+(The function call would actually be in your main Python
+program, not grouped with the function in the module,
+but don't worry about that.) Remember that you need a
+variable for storing the returned value. Make up the
+name of the function and the name of the variable.
+When calling the function, assume that the module has
+already been imported.
+'''
+
+def addInts(a, b):
+  return a + b
+storeVal = calculations.addInts(8, 9)
+
