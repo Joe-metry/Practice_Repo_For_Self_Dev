a8079bfbd1a2dd682cd384442ee976281400a88d
Joe-metry <joemetry150@gmail.com>
2023-05-15 10:02:37 -0700

    Task 2 on Exception Handling

---
+++ Python_Book_Exercises/exceptionB.py
@@ -0,0 +1,15 @@
+'''
+Code the next line to deal with the file not being found.
+try:
+  with open("xyz.json", "w") as j:
+'''
+except FileNotFoundError:
+
+'''
+  Code the first two lines of a block with exception handling.
+  The second line opens a text file for reading.
+  Use shorthand. Make up what you need to.
+'''
+
+try:
+  with open("the_file.txt") as J:
