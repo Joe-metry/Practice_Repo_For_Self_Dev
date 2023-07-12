51a8840c9d42bb0dcf5511e07501630325386890
Joe-metry <joemetry150@gmail.com>
2023-07-12 11:00:55 -0700

    Task 8

---
+++ Pre_Sql/udacity8.sql
@@ -0,0 +1,11 @@
+/*
+  Which month did Parch & Posey have the greatest sales in terms of total dollars?
+  Are all months evenly represented by the dataset?
+
+*/
+
+SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_spent
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC; 
